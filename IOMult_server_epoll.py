b2ea4a7f561c0cb223a73bdf10cbd4d151dec906
Fucloud <270928474@qq.com>
2023-04-13 17:50:43 +0800

    上传 epoll实现代码

--- IOMult_server_epoll.py
+++ IOMult_server_epoll.py
@@ -14,7 +14,7 @@ def main():
     # 创建一个epoll对象
     epoll = select.epoll()
     # 为服务器端套接字server_socket的文件描述符注册事件
-    epoll.register(...)
+    epoll.register(server_socket.fileno(), select.EPOLLIN)
     
     new_socket_list = {}       
     client_address_list = {}     
@@ -22,21 +22,31 @@ def main():
     # 循环等待数据到达
     while True:
         # 检测并获取epoll监控的已触发事件
-        epoll_list = ...
+        epoll_list = epoll.poll(1)
         # 对事件进行处理
-        for fd, events in epoll_list:
+        for fd, event in epoll_list:
             # 如果有新的连接请求递达
             if fd == server_socket.fileno():
                 new_socket, client_address = server_socket.accept()
                 print('有新的客户端到来%s'%str(client_address))
                 
                 # 为新套接字的文件描述符注册读事件
-                ...
-
+                new_socket.setblocking(0)
+                epoll.register(new_socket.fileno(), select.EPOLLIN)
+                new_socket_list[new_socket.fileno()] = new_socket
+                client_address[new_socket.fileno()] = client_address
             # 如果监听到EPOLLIN事件, 表示对应的文件描述符可以读
-            elif ...:
+            elif event & select.EPOLLIN:
                 # 处理逻辑
-                ...
+                msg = new_socket_list[fd].recv(1024).decode()
+                new_socket_list[fd].send(msg.upper().encode())
+                
+                epoll.modify(fd, select.EPOLLOUT)
+                new_socket_list.pop(fd)
+                client_address.pop(fd)
+
+                
+
 
 
 if __name__ == '__main__':
